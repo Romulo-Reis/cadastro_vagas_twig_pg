2636868305f6ebf63d3037a4721c4203ca77e6bc
Romulo-Reis <romuloreistavates@hotmail.com>
2021-10-14 22:41:22 -0300

    Este commit altera as classes LoginController e UsuarioController para corrigir a atribuição do status do usuário.

--- App/Controllers/LoginController.php
+++ App/Controllers/LoginController.php
@@ -70,7 +70,7 @@ class LoginController extends Controller
             $usuario->setSenha($_POST['senha']);
             $usuario->setConfSenha($_POST['confSenha']);
             $usuario->setEmail($_POST['email']);
-            $usuario->setStatus(false);
+            $usuario->setStatus(0);
             $dataAtual = DataUtil::getDataAtual();
             $usuario->setDataCadastro(DataUtil::getDataFormatoATOM($dataAtual));
             Sessao::gravaFormulario($_POST);
