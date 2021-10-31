d4a81100263b0306a82c07db5ea0c9051a8a7c58
Romulo-Reis <romuloreistavates@hotmail.com>
2021-10-31 18:26:40 -0300

    Adicionar o script de configiração do dataTables da listar de perfis

--- public/js/common-scripts.js
+++ public/js/common-scripts.js
@@ -38,6 +38,13 @@ $(function() {
             }
         }
     );
+    $('#lista-perfis').DataTable(
+        {
+            language: {
+                url: '//cdn.datatables.net/plug-ins/1.11.3/i18n/pt_br.json'
+            }
+        }
+    );
 });
 
 var Script = function () {
